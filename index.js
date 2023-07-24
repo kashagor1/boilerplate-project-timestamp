e154fc5603a0580e0038561ec66e38c16fe0ef82
unknown <ka.shagor1@gmail.com>
2023-07-24 17:31:11 +0600

    readded env.port

--- index.js
+++ index.js
@@ -67,6 +67,6 @@ app.get("/api/:time", function (req, res) {
 
 
 // listen for requests :)
-var listener = app.listen(3000, function () {
+var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
